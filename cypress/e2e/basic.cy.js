7c6ae1236134821a76649903a2ac92fdf97234ed
melgadev <melgarejo.developer@gmail.com>
2022-11-13 21:54:03 -0300

    Fixes test

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -4,7 +4,7 @@ describe('empty spec', () => {
   })
 
   it('opens the index page', () => {
-    cy.get('h1').contains('Contactate con nosotros')
+    cy.get('h1').contains('Mecon Construcciones')
   })
 
   it('navigates to the obras page', () => {
