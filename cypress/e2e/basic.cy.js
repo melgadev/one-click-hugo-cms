1cf13b7843b2d6e3888e6b65d4d444f2f45ce97c
melgadev <melgarejo.developer@gmail.com>
2022-11-13 21:48:01 -0300

    Updates tests

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -4,31 +4,14 @@ describe('empty spec', () => {
   })
 
   it('opens the index page', () => {
-    cy.get('h1').contains('Great coffee with a conscience')
+    cy.get('h1').contains('Contactate con nosotros')
   })
 
-  it('navigates to the product page', () => {
-    cy.get('a[href="/products"]').eq(0).click();
-    cy.url().should("include", "/products")
-    cy.get('h1').contains(/Our Coffee/i)
+  it('navigates to the obras page', () => {
+    cy.get('a[href="/obras"]').eq(0).click();
+    cy.url().should("include", "/obras")
+    cy.get('h1').contains(/Obras/i)
   })
 
-  it('navigates to the values page', () => {
-    cy.get('a[href="/values"]').eq(0).click();
-    cy.url().should("include", "/values")
-    cy.get('h1').contains(/Values/i)
-  })
-
-  it('navigates to the blog page', () => {
-    cy.get('a[href="/post"]').eq(0).click();
-    cy.url().should("include", "/post")
-    cy.get('h1').contains(/Latest Stories/i)
-  })
 })
 
-describe('validate blog', () => {
-  it('should have only 3 blog posts by default', () => {
-    cy.visit('/post')
-    cy.get('ul#blog-list li').should('have.length', 3);
-  })
-})
